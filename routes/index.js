448c8e0b55ead89e112a40f6f6eab8c39c7e9909
empobla-mac <A01027265@itesm.mx>
2021-01-10 00:07:08 -0600

    Added Events and fixed footer

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,12 @@ router.get('/inicio', function(req, res, next) {
   res.render('index', { title: 'Gira' });
 });
 
+router.get('/eventos', function(req, res, next) {
+  res.render('events', { title: 'Gira: Eventos' });
+});
+
+router.get('/eventos/elecciones-2021-mexico', function(req, res, next) {
+  res.render('event', { title: 'Gira: Elecciones 2021 México' });
+});
+
 module.exports = router;
